0320677f49878a78955996ec26e08672bacb211e
ginkoprim <93119906+ginkoprim@users.noreply.github.com>
2024-09-27 09:34:36 +0300

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>International Integration Developer</title>
+          <meta
+            property="og:title"
+            content="International Integration Developer"
+          />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
